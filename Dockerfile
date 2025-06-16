4e92387b5f2b2e965d53c57dd1954ba17d3b0ebc
Florian <florian@uplevl.ai>
2025-06-16 06:42:07 +0200

    chore: fixing deployment

--- Dockerfile
+++ Dockerfile
@@ -39,4 +39,4 @@ COPY --from=build /app /app
 
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 3000
-CMD [ "bun", "start" ]
+CMD [ "bun", "index.ts" ]
--- app/env.ts
+++ app/env.ts
@@ -2,7 +2,7 @@ import { z } from "zod/v4";
 
 const EnvSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("development"),
-  APP_PORT: z.coerce.number().optional().default(8080),
+  APP_PORT: z.coerce.number().optional().default(3000),
   DATABASE_URL: z.string().url(),
   DB_MIGRATING: z.coerce.boolean().optional().default(false),
   DB_SEEDING: z.coerce.boolean().optional().default(false),
